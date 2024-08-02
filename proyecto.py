a0074993c6c6fd0b71773843a4c8d899f14f0841
MTJonathan <jonathan02330@gmail.com>
2024-08-02 11:22:17 -0500

    Variables y tipos de datos

--- proyecto.py
+++ proyecto.py
@@ -1,1 +1,12 @@
-print("Hola mundo")
+nombre = input("Ingresa tu nombre: ")
+numero = input("Numero: ")
+numero2 = input("Numero2: ")
+numero3 = input("Numero3: ")
+print(f"Hola mundo {nombre}")# Esto es un comentario en python
+
+if(numero > numero2 and numero > numero3):
+    print(f"{numero} es mayor que {numero2} y {numero3}")
+elif(numero2 > numero3 and numero2 > numero):
+    print(f"{numero2} es mayor que {numero} y {numero3}")
+else:
+    print(f"{numero3} es mayor que {numero} y {numero2}")
---
+++ variables.py
@@ -0,0 +1,12 @@
+integer = 32 #Variable de tipo entero
+float_var = 3.2 #Variable de tipo flotante
+string = 'Esto es un texto' #Esto es una cadena de texto
+boolean = True #Variable de tipo booleano
+
+print ("")
+print ("Esto son algunas Tipos de Datos en python : ")
+print ("")
+print(f"Numero entero: {integer}")
+print(f"Numero decimal: {float_var}")
+print(f"Cadena de texto: {string}")
+print(f"Booleana: {boolean}")
